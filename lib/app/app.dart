f5880283833c8ccda0de0d3b758ea33277da0f10
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-10-26 19:15:58 +0700

    refactor(app): config home to LoginPage

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,1 +1,22 @@
-export 'view/app.dart';
+import 'package:event_app/features/auth/presentation/pages/login_page.dart';
+import 'package:event_app/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const LoginPage(),
+    );
+  }
+}
