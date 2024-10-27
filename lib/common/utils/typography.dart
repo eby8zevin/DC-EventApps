b032d483d5b9ad93cd19f3ae7f3bab1c1b1ec0f6
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-10-27 09:31:36 +0700

    feat(utils): create typography

---
+++ lib/common/utils/typography.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTypography {
+  static final body = GoogleFonts.poppins().copyWith(
+    fontSize: 12,
+    fontWeight: FontWeight.w500
+  );
+  static final title = GoogleFonts.poppins().copyWith(
+    fontSize: 14,
+    fontWeight: FontWeight.w500
+  );
+  static final heading = GoogleFonts.poppins().copyWith(
+    fontSize: 18,
+    fontWeight: FontWeight.w600
+  );
+}
