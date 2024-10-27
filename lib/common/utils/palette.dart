2dd7df2d06248343bb8a0d5d4acc67fb033be8cc
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-10-27 08:25:23 +0700

    feat(utils): create palette color

---
+++ lib/common/utils/palette.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const primary = Color(0xff21007D);
+  static const secondary = Color(0xff6A46AA);
+  static const green = Color(0xff07A333);
+  static const red = Color(0xffC52A2C);
+  static const purple = Color(0xffBAC2FF);
+  static const blue = Color(0xff4E91FD);
+  static const grey = Color(0xffD9D9D9);
+  static const orange = Color(0xffFFB417);
+}
