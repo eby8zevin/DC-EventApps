6ff2637d0c0f08d2a776f3974cf0df61dd5dffe9
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-10-28 07:23:44 +0700

    refactor(main_dev): add console log flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,21 @@
+import 'dart:developer';
+
 import 'package:event_app/app/app.dart';
 import 'package:event_app/bootstrap.dart';
+import 'package:event_app/core/env/env_config.dart';
+import 'package:event_app/core/env/flavors.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
+  FlavorSettings.development();
+
   bootstrap(() => const App());
+
+  /// console flavor running
+  if (!kReleaseMode) {
+    final setting = EnvConfig.getInstance();
+
+    log('${setting!.flavorName}', name: 'Flavor Name');
+    log('${setting.apiBaseUrl}', name: 'BASE URL');
+  }
 }
